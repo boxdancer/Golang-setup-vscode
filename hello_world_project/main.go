6ec90d8abb3f07a252e30eb57b23f907ea3d2a63
tselinap <tselinap@mos-team.ru>
2025-03-17 21:02:23 +0300

    setup commit

---
+++ hello_world_project/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	result := (16 * 16) + (12 * 16) + 3
+	result = (16 * 16) + (12 * 16) + 3
+	result = (16 * 16) + (12 * 16) + 3
+	fmt.Println(result)
+}
