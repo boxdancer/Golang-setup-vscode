ae002c0f02ba2b004ea8330293d0fbe7817bd943
tselinap <tselinap@mos-team.ru>
2025-03-23 20:49:07 +0300

    feat(Go-course): pointers

--- hello_world_project/main.go
+++ hello_world_project/main.go
@@ -6,7 +6,19 @@ import (
 
 func main() {
 	num := 15
+	// &num - pointer to num, shows the memory address of num
 
-	numStr := fmt.Sprintf("Запись числа %d в разных системах счисления:\nДесятичная: %d\nДвоичная: %b\nВосьмеричная: %o\nШестнадцатеричная: %X", num, num, num, num, num)
-	fmt.Println(numStr)
+	num2 := num
+	fmt.Println(num2, &num2, num)
+
+	num2 = 16
+	fmt.Println(num2, &num2, num)
+
+	num3 := &num
+	// * используется для получения значения по указателю
+	fmt.Println(num3, &num3, *num3)
+
+	// *num используется для изменения значения по указателю
+	*num3 = 17
+	fmt.Println(num, num2, *num3)
 }
