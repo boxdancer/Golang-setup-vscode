e5be2c82c58e8a8e8706b3245c064a0629d404a2
tselinap <tselinap@mos-team.ru>
2025-03-27 20:16:53 +0300

    if else block

--- hello_world_project/main.go
+++ hello_world_project/main.go
@@ -5,14 +5,16 @@ import (
 )
 
 func main() {
-	random := 5.9
-	strNumber := fmt.Sprintf("%.1f", random)
-	strNumberIncr := fmt.Sprintf("%.5f", random*1.1)
-	isEven := int(random)%2 == 0
-	predLastDigit := (int(random) / 10) % 10
-	fmt.Println(
-		"Исходное число:", strNumber,
-		"\nИсходное число, увеличенное на 10%:", strNumberIncr,
-		"\nИсходное число является четным:", isEven,
-		"\nПредпоследняя цифра целой части исходного числа:", predLastDigit)
+	age := 10
+	role := "user"
+	status := "active"
+
+	var flag bool
+
+	if role == "admin" || role == "moderator" || age >= 18 && role == "user" && status == "active" {
+		flag = true
+	} else {
+		flag = false
+	}
+	fmt.Println(flag)
 }
